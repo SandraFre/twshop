732bb25fdfd22356409abbd472e7effb95d9cf58
SandraFre <SandraFre@users.noreply.github.com>
2020-07-26 20:03:45 +0300

    Register controller created

--- app/Admin.php
+++ app/Admin.php
@@ -13,6 +13,24 @@ use Illuminate\Notifications\Notifiable;
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Admin whereUpdatedAt($value)
  */
 class Admin extends Authenticatable
 {
