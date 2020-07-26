6b41b253d34430d03359fcf1a287adf477203b37
SandraFre <SandraFre@users.noreply.github.com>
2020-07-26 20:36:10 +0300

    web.php edited

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->group(function(){
+
+});
